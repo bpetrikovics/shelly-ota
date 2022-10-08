b6c320957f901d53c3e132b7fc47c24f1819a339
Balazs Petrikovics <bpetrikovics@gmail.com>
2022-10-08 14:02:52 +0200

    More tests

--- test_auth.py
+++ test_auth.py
@@ -44,20 +44,40 @@ def multi_auth_provider(multi_args):
     return AuthProvider(multi_args)
 
 @pytest.fixture
-def dummy_device():
+def dummy_device1():
     return ShellyDevice(
-        '1.2.3.4',
+        '0.0.0.0',
         {'type': 'DUMMY', 'mac': '00:00:00:00:00:00'}
         )
 
+@pytest.fixture
+def dummy_device2():
+    return ShellyDevice(
+        '1.1.1.1',
+        {'type': 'DUMMY', 'mac': '01:01:01:01:01:01'}
+        )
 
-def test_no_auth(no_auth_provider, dummy_device):
+@pytest.fixture
+def dummy_device3():
+    return ShellyDevice(
+        '2.2.2.2',
+        {'type': 'SHPLG-S', 'mac': '02:02:02:02:02:02'}
+        )
+
+
+def test_no_auth(no_auth_provider, dummy_device1):
     assert no_auth_provider is not None
     assert no_auth_provider.get_auth_for(None) is None
-    assert no_auth_provider.get_auth_for(dummy_device) is None
+    assert no_auth_provider.get_auth_for(dummy_device1) is None
 
-def test_simple_auth(simple_auth_provider, dummy_device):
-    assert simple_auth_provider.get_auth_for(dummy_device) == ('user', 'pass')
+def test_simple_auth(simple_auth_provider, dummy_device1):
+    assert simple_auth_provider.get_auth_for(dummy_device1) == ('user', 'pass')
 
-def test_multi_auth(multi_auth_provider, dummy_device):
-    assert multi_auth_provider.get_auth_for(dummy_device) == ('test1_user', 'test1_pass')
+def test_multi_auth1(multi_auth_provider, dummy_device1):
+    assert multi_auth_provider.get_auth_for(dummy_device1) == ('test1_user', 'test1_pass')
+
+def test_multi_auth2(multi_auth_provider, dummy_device2):
+    assert multi_auth_provider.get_auth_for(dummy_device2) == ('test2_user', 'test2_pass')
+
+def test_multi_auth3(multi_auth_provider, dummy_device3):
+    assert multi_auth_provider.get_auth_for(dummy_device3) == ('test3_user', 'test3_pass')
